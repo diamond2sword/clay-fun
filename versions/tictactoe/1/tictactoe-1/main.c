12350e1ca028a81da8c9ccf62f3adc17b757db13
diamond2sword <diamond2sword@gmail.com>
2025-01-13 15:35:00 +0800

    added project versions

--- versions/tictactoe/1/tictactoe-1/main.c
+++ versions/tictactoe/1/tictactoe-1/main.c
@@ -147,7 +147,7 @@
 #define CLAY_EXTEND_CONFIG_IMAGE Clay_String sourceURL;
 #define CLAY_EXTEND_CONFIG_TEXT bool disablePointerEvents;
 #define CLAY_IMPLEMENTATION
-#include "../clay/clay.h"
+#include "clay.h"
 
 typedef enum : uint8_t {
 	CLICK_PHASE_NONE,
