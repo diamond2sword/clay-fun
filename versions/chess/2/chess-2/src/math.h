9ec93eb8a96d6da1896b985468f8a1ad31e87501
diamond2sword <diamond2sword@gmail.com>
2025-01-22 22:22:59 +0800

    Refactoring chess from Chess with board array to using Bitboards

---
+++ versions/chess/2/chess-2/src/math.h
@@ -0,0 +1,13 @@
+#ifndef MY_MATH
+#define MY_MATH
+float floorf(float x);
+#endif // MY_MATH
+
+#ifdef MY_MATH_IMPLEMENTATION
+#undef MY_MATH_IMPLEMENTATION
+float floorf(float x)
+{
+    int i = (int)x;
+    return (x < 0 && x != i) ? i - 1 : i;
+}
+#endif // MY_MATH_IMPLEMENTATION
