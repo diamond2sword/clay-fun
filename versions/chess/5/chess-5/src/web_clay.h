e56050ce74e49774a9c3efec9ffc7dc529b42ecc
diamond2sword <diamond2sword@gmail.com>
2025-01-24 20:16:27 +0800

    Polished All Logics Only With Pawn Before Moving Forwars. Thanks be to God.

---
+++ versions/chess/5/chess-5/src/web_clay.h
@@ -0,0 +1,10 @@
+#ifndef WEB_CLAY_HEADER
+#define WEB_CLAY_HEADER
+
+#define CLAY_EXTEND_CONFIG_RECTANGLE Clay_String link; bool cursorPointer;
+#define CLAY_EXTEND_CONFIG_IMAGE Clay_String sourceURL; Clay_BoundingBox subrectangle; 
+#define CLAY_EXTEND_CONFIG_TEXT bool disablePointerEvents;
+
+#endif // WEB_CLAY_HEADER
+
+#include "clay.h"
